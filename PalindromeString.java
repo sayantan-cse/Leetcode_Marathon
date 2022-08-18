aef567444ef6e2df1ada5b42b6cc3e2f9dfc0912
sayantan-cse <111502452+sayantan-cse@users.noreply.github.com>
2022-08-18 16:38:02 +0530

    Create PalindromeString.java

---
+++ PalindromeString.java
@@ -0,0 +1,52 @@
+import com.sun.jdi.event.EventSet;
+
+// Java program to check whether a string is a Palindrome
+//Naive approach
+
+// class PalindromeString{
+
+//   public static boolean isPS(String str){
+
+//     //initialising  reverse string with empty string
+//     String reverse = "";
+
+//     for (int i =str.length()-1 ; i >=0 ;i--){
+//       reverse= reverse + str.charAt(i);
+//       }
+//     if (str.equals(reverse)){
+//       return true;
+//     }
+//     return false;
+//   }
+// }
+
+// class Main{
+//   public static void main(String[] args){
+//     String teststr = "sayantancse";
+//     boolean isPalindrome=PalindromeString.isPS(teststr);
+//     System.out.println(isPalindrome);
+//   }
+// }
+
+//recursive approach
+//two pointers
+
+class PalindromeStringRecursive{
+  public static boolean isPalindromeRecursive(int start, int end,String str){ 
+    if(start >= end){
+      return true;
+    }
+    if (str.charAt(start)!=str.charAt(end)){
+      return false;
+    }
+    return isPalindromeRecursive(start+1,end-1,str);
+}
+  
+}
+class Main{
+  public static void main(String[] args){
+    String teststr = "ada";
+    boolean  ispal = PalindromeStringRecursive.isPalindromeRecursive(0, teststr.length()-1,teststr);
+    System.out.println(ispal);
+  }
+}
